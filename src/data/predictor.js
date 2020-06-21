dfc2ae687929af97b87911b93bb4189d9980da79
Harriet Goddard <goddardhe@gmail.com>
2020-06-21 14:15:34 +0100

    Comments added to reducer

--- src/data/predictor.js
+++ src/data/predictor.js
@@ -1,5 +1,6 @@
 /*
-* takes a team (an array of player objects) and returns the total skills value
+* takes a team (an array of player objects) and returns the total
+* skills value
 */
 
 export const totalTeamSkills = team => {
@@ -8,15 +9,24 @@ export const totalTeamSkills = team => {
 
 
 /*
-* returns a new total skills value augmented to reflect the home advantage
+* returns a new total skills value augmented to reflect the home 
+* advantage
 */
 
 export const addHomeAdvantage = totalSkills => {
-    let advantage = totalSkills * 0.15;
+    let advantage = totalSkills * 0.15; //update with variable
     return totalSkills + advantage; 
 };
 
+
+/*
+* takes two teams (arrays of player objects) and a number
+* representing the home team, and returns an object
+* containing the teams' total skills values
+*/
+
 export const calculateScores = (team1, team2, home) => {
+    
     let team1Total = home === 1 ? addHomeAdvantage(totalTeamSkills(team1)) : totalTeamSkills(team1);
 
     let team2Total = home === 2 ? addHomeAdvantage(totalTeamSkills(team2)) : totalTeamSkills(team2);
@@ -28,17 +38,10 @@ export const calculateScores = (team1, team2, home) => {
 };
 
 /*
-* returns 
+* takes a scores object (see above) and returns a number
+* representing the favourite team
 */
 
 export const calculateFavourite = scores => {
     return Math.max(scores.team1Total, scores.team2Total) === scores.team1Total ? 1 : 2;
-}
-
-// export const calculateFavourite = (team1, team2, home) => {
-//     let team1Total = home === 1 ? addHomeAdvantage(totalTeamSkills(team1)) : totalTeamSkills(team1);
-
-//     let team2Total = home === 2 ? addHomeAdvantage(totalTeamSkills(team2)) : totalTeamSkills(team2);
-    
-//     return Math.max(team1Total, team2Total) === team1Total ? 1 : 2;
-// };
+};
--- src/data/reducer.js
+++ src/data/reducer.js
@@ -2,6 +2,11 @@ import initialState from './initial';
 import { randomTeams, balancedTeams } from './teamPicker';
 import { calculateFavourite, calculateScores } from './predictor';
 
+
+/*
+* increase or decrease the number of players per team
+*/
+
 const changeNoOfPlayers = (state, { value }) => {
     return {
         ...state,
@@ -9,22 +14,43 @@ const changeNoOfPlayers = (state, { value }) => {
     };
 };
 
+
+/*
+* add a player (name and skill)
+*/
+
 const addPlayer = (state, { data }) => {
     return {
         ...state,
-        players: [...state.players, { name: data.playerName, skill: data.playerSkill }]
+        players: [
+            ...state.players, 
+            { name: data.playerName, skill: data.playerSkill }
+        ]
     };
 };
+
+
+/*
+* check whether enough players have been added given
+* the specified number of players per team
+*/
 
 const checkNoOfPlayers = state => {
     
     const currentNoOfPlayers = state.players.length;
     
+    //check number of players added is twice that of the number of players per team
     const enoughPlayers = currentNoOfPlayers === state.numberOfPlayers * 2;
 
     return { ...state, playerInputComplete: enoughPlayers };
 
 };
+
+
+/*
+* generate teams, depending on whether the user has opted
+* for random or balanced team allocation. See teamPicker.js
+*/
 
 const generateTeams = state => {
 
@@ -32,8 +58,9 @@ const generateTeams = state => {
 
     const teams = state.balanced ? balancedTeams(players, 2) : randomTeams(players, 2);
 
+    //allocate the resulting array of arrays to teams
+    //***this needs to be randomised***
     const team1 = teams[0];
-
     const team2 = teams[1];
 
     return {
@@ -44,12 +71,22 @@ const generateTeams = state => {
 
 };
 
+
+/*
+* toggle between random and balanced team allocation
+*/
+
 const toggleBalanced = state => {
     return {
         ...state,
         balanced: !state.balanced,
     };
 };
+
+
+/*
+* toggle the home team
+*/
 
 const toggleHome = state => {
     let currentHomeTeam = state.home;
@@ -60,18 +97,31 @@ const toggleHome = state => {
     };
 };
 
+
+/*
+* calculate the favourite team, whether the favourite team
+* had the home advantage, the favourite team's total
+* skills value and the other team's total skills value.
+* See predictor.js
+*/
+
 const getPredictions = state => {
     
     const { playersTeam1, playersTeam2, home } = state;
 
+    //returns an object containing the teams total skills values
     let scores = calculateScores(playersTeam1, playersTeam2, home);
 
+    //returns a number representing the favourite team
     let favourite = calculateFavourite(scores);
 
+    //did the favourite benefit from a home advantage
     let favouriteHome = favourite === home;
 
+    //returns the favourite team's total skills value
     let favouriteSkills = favourite === 1 ? scores.team1Total : scores.team2Total;
 
+    //returns the other team's total skills value
     let unfavouriteSkills = favourite === 1 ? scores.team2Total : scores.team1Total;
 
     return {
