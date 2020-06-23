455731a80ba65f1f90b1ec7e0bef5c7b036a80f9
Harriet Goddard <goddardhe@gmail.com>
2020-06-23 10:17:07 +0100

    player card styling altered

--- src/components/PlayerList/PlayerList.js
+++ src/components/PlayerList/PlayerList.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PlayerList = ({ players, handleClick }) => {
+class PlayerList extends Component  {
 
-    return (
-        <div className="player-list-container">    
-            <ul id="playerList" className="player-list">
-                { players.map((player,index) => {
-                    return (
-                        <li className="player-card" key={ index }>
-                            <p className="text-card">{ player.name }</p>
-                            <p className="text-card">Skill { player.skill }</p>
-                            <button
-                                className="btn-delete"
-                                onClick={ () => handleClick(index) }
-                            >x</button>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    );
+    constructor(props) {
+        super(props);
+    };
+
+    //to keep the player list scroll bar at the bottom of the container when adding players
+    componentDidUpdate() {
+        
+        const { playerList } = this.refs;
+        
+        playerList.scrollTop = playerList.scrollHeight - playerList.clientHeight;
+    }
+
+    render() {
+        
+        const { players, handleClick } = this.props;
+
+        return (
+            <div className="player-list-container">    
+                <ul ref="playerList" className="player-list">
+                    { players.map((player,index) => {
+                        return (
+                            <li className="player-card" key={ index }>
+                                <p className="text-card">{ player.name }</p>
+                                <p className="text-card">Skill { player.skill }</p>
+                                <button
+                                    className="btn-delete"
+                                    onClick={ () => handleClick(index) }
+                                >x</button>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        );
+    };
 
 };
 
