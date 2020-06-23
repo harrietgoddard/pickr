f70c5ed657e34bc3d10d488cf8bbc5749b08d4ea
Harriet Goddard <goddardhe@gmail.com>
2020-06-23 10:25:12 +0100

    team list styled

--- src/components/TeamList/TeamList.js
+++ src/components/TeamList/TeamList.js
@@ -3,12 +3,12 @@ import React from 'react';
 const TeamList = ({ playersTeam }) => {
 
     return (
-            <ul>
+            <ul className="team-list">
                 { playersTeam.map((player, index) => {
                     return (
-                        <li style={{display:"flex"}} key={ index }>
-                            <p>{ player.name }</p>
-                            <p>{ player.skill }</p>
+                        <li className="player-card" key={ index }>
+                            <p className="text-card">{ player.name }</p>
+                            <p className="text-card">Skill { player.skill }</p>
                         </li>
                     )
                 })}
