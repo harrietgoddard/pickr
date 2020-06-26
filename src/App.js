58895455a8221a06202041ae66ab61ad883ad770
Harriet Goddard <goddardhe@gmail.com>
2020-06-26 11:36:55 +0100

    Tweaks to spacing in data files

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
     </div>
 
   );
-}
+  
+};
 
 export default App;
--- src/data/action.js
+++ src/data/action.js
@@ -1,73 +1,94 @@
 export const changeNumber = value => {
+
     return {
         type: "CHANGE_NO_OF_PLAYERS",
         value,
     };
+
 };
 
 export const addPlayer = data => {
+
     return {
         type: "ADD_PLAYER",
         data,
     };
+
 };
 
 export const deletePlayer = index => {
+
     return {
         type: "DELETE_PLAYER",
         index,
-    }
-}
+    };
+
+};
 
 export const reset = () => {
+
     return {
         type: "RESET",
     };
+
 };
 
 export const generateTeams = () => {
+
     return {
         type: "GENERATE_TEAMS",
     };
+
 };
 
 export const toggleBalanced = () => {
+
     return {
         type: "TOGGLE_BALANCED",
     };
+
 };
 
 export const toggleHome = () => {
+
     return {
         type: "TOGGLE_HOME",
     };
+
 };
 
 export const getPredictions = () => {
+
     return {
         type: "GET_PREDICTIONS",
     };
+
 };
 
 export const showTeams = () => {
+
     return {
         type: "SHOW_TEAMS",
     };
+
 };
 
 export const setColor1 = data => {
+
     return {
         type: "SET_COLOR",
         team: 1,
         color: data,
     };
+
 };
 
 export const setColor2 = data => {
+
     return {
         type: "SET_COLOR",
         team: 2,
         color: data,
     };
+    
 };
-
