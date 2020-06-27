90a60c23753f176ee383a5d3d3fedfa73cbf27d2
Harriet Goddard <goddardhe@gmail.com>
2020-06-27 18:23:13 +0100

    Updates

---
+++ precache-manifest.f074e61d66d331be1be487ab01c5c449.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6debc6323dc2815d662ebac1381389",
+    "url": "/pickr/index.html"
+  },
+  {
+    "revision": "52663aa114ddf5eff402",
+    "url": "/pickr/static/css/main.3388e5f9.chunk.css"
+  },
+  {
+    "revision": "85978d603b360c832649",
+    "url": "/pickr/static/js/2.75028eb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pickr/static/js/2.75028eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52663aa114ddf5eff402",
+    "url": "/pickr/static/js/main.6cf744cc.chunk.js"
+  },
+  {
+    "revision": "03dc84331b964318d21d",
+    "url": "/pickr/static/js/runtime-main.9fe45863.js"
+  },
+  {
+    "revision": "dd5f924c87411c05e4d83f2d04765f2a",
+    "url": "/pickr/static/media/football-team-med.dd5f924c.jpg"
+  },
+  {
+    "revision": "28c75379348698805bae2accb077f436",
+    "url": "/pickr/static/media/football-team-small.28c75379.jpg"
+  },
+  {
+    "revision": "7cfe0a73185762d39e17c6315883165d",
+    "url": "/pickr/static/media/football-team.7cfe0a73.jpg"
+  },
+  {
+    "revision": "f2ddd09a156635b2503d57893bec3ab4",
+    "url": "/pickr/static/media/tshirt.f2ddd09a.svg"
+  }
+]);
